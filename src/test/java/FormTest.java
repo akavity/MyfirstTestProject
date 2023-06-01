54c0653aac5fb51ebe24e84e8f02f312ec197e21
Basil Akavity <vakavity@gmail.com>
2023-06-01 15:18:23 +0300

    change FormTest

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,40 +1,25 @@
+import org.example.driver.DriverManager;
 import org.example.pages.RegistrationFormPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
-public class FormTest {
-    private WebDriver driver;
-    private WebDriverWait wait;
+public class FormTest extends BaseTest {
+    protected WebDriver driver;
     private RegistrationFormPage registrationFormPage;
 
     @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "G:\\Installation\\chromedriver.exe");
-        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        driver.manage().window().maximize();
-        driver.get("https://demoqa.com/automation-practice-form");
-        registrationFormPage = new RegistrationFormPage(driver, wait);
+    public void preparationForTest() {
+        driver = DriverManager.getDriver();
+        registrationFormPage = new RegistrationFormPage(driver);
     }
 
-    @DataProvider(name = "data-provider")
-    public Object[][] dpMethod() {
-        return new Object[][]{{"Basil"}, {"Alex"}};
-    }
-
-    @Test(dataProvider = "data-provider")
-    public void checkRegistrationFormData(String name) {
-        registrationFormPage.enterFirstName(name);
+    @Test
+    public void checkRegistrationFormData() {
+        registrationFormPage.enterFirstName("Basil");
         registrationFormPage.enterLastName("Akavity");
         registrationFormPage.clickButton();
         registrationFormPage.enterMobilePhoneNumber("7529545499");
@@ -47,10 +32,5 @@ public class FormTest {
 
         Assert.assertTrue(userDataText.getText().contains("Basil Akavity") ||
                 userDataText.getText().contains("Alex Akavity"));
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
     }
 }
