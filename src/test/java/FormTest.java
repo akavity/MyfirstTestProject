146e928b424844fdf7d5b1a1b0b9fbe6d10983b0
Basil Akavity <vakavity@gmail.com>
2023-06-01 09:17:28 +0300

    change FormTest

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,20 +1,21 @@
+import org.example.pages.RegistrationFormPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class FormTest {
-
     private WebDriver driver;
     private WebDriverWait wait;
+    private RegistrationFormPage registrationFormPage;
 
     @BeforeMethod
     public void setUp() {
@@ -23,34 +24,29 @@ public class FormTest {
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         driver.manage().window().maximize();
         driver.get("https://demoqa.com/automation-practice-form");
+        registrationFormPage = new RegistrationFormPage(driver, wait);
     }
 
-    @Test
-    public void checkRegistrationFormData() {
+    @DataProvider(name = "data-provider")
+    public Object[][] dpMethod() {
+        return new Object[][]{{"Basil"}, {"Alex"}};
+    }
 
-        WebElement firstNameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("firstName")));
-//      WebElement firstNameField = driver.findElement(By.id("firstName"));
-        firstNameField.sendKeys("Basil");
-
-        WebElement lastNameField = driver.findElement(By.xpath("//input[@placeholder=\"Last Name\"]"));
-        lastNameField.sendKeys("Akavity");
-
-        WebElement maleRadioButton = driver.findElement(By.xpath("//label[@for=\"gender-radio-1\"]"));
-        maleRadioButton.click();
-
-        WebElement mobilePhoneNumberField = driver.findElement(By.xpath("//input[@placeholder=\"Mobile Number\"]"));
-        mobilePhoneNumberField.sendKeys("7529545499");
-
-        WebElement uploadPictureInput = driver.findElement(By.id("uploadPicture"));
-        uploadPictureInput.sendKeys("G:\\home\\akavity\\dev\\course-qa-automation-engineer\\MyFirstTestProject\\src\\main\\resources\\2.jpg");
-
-        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type=\"submit\"]")));
-//      WebElement submitButton = driver.findElement(By.xpath("//button[@type=\"submit\"]"));
-        submitButton.click();
+    @Test(dataProvider = "data-provider")
+    public void checkRegistrationFormData(String name) {
+        registrationFormPage.enterFirstName(name);
+        registrationFormPage.enterLastName("Akavity");
+        registrationFormPage.clickButton();
+        registrationFormPage.enterMobilePhoneNumber("7529545499");
+        registrationFormPage.clickDropDown("NCR");
+        registrationFormPage.uploadPictureInput("G:\\home\\akavity\\dev\\course-qa-automation-engineer\\" +
+                "MyFirstTestProject\\src\\main\\resources\\2.jpg");
+        registrationFormPage.clickSubmitButton();
 
         WebElement userDataText = driver.findElement(By.xpath(".//tr[1]/td[1]/following-sibling::td"));
 
-        Assert.assertTrue(userDataText.getText().contains("Basil Akavity"));
+        Assert.assertTrue(userDataText.getText().contains("Basil Akavity") ||
+                userDataText.getText().contains("Alex Akavity"));
     }
 
     @AfterMethod
