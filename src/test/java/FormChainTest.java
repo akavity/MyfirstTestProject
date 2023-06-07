048c6be1aad7bdc9ea5a31f3b15ef00cf2bbb70d
Basil Akavity <vakavity@gmail.com>
2023-06-07 16:41:12 +0300

    add JsonReader

--- src/test/java/FormChainTest.java
+++ src/test/java/FormChainTest.java
@@ -1,23 +1,15 @@
 import org.example.driver.DriverManager;
 import org.example.models.UserData;
 import org.example.pages.RegistrationFormChainPage;
+import org.example.utils.JsonReader;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class FormChainTest extends BaseTest {
     protected WebDriver driver;
     private RegistrationFormChainPage registrationFormPage;
-
-    @DataProvider(name = "data-provider")
-    public Object[][] dpMethod() {
-        return new Object[][]{
-                {new UserData("Alex", "Akavity")},
-                {new UserData("Basil", "Akavity")}
-        };
-    }
 
     @BeforeClass
     public void preparationForTest() {
@@ -25,7 +17,7 @@ public class FormChainTest extends BaseTest {
         registrationFormPage = new RegistrationFormChainPage(driver);
     }
 
-    @Test(dataProvider = "data-provider")
+    @Test(dataProvider = "userData", dataProviderClass = JsonReader.class)
     public void checkRegistrationFormDataChain(UserData userData) {
 
         String userDataText = registrationFormPage
